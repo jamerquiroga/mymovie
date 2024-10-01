dff8e7f0d3f793ab176b5b1344fd5e48d4a41ed7
Jamer Quiroga <jamerquiroga@gmail.com>
2024-09-30 21:52:45 -0500

    AppBar y Scaffold sample

--- app/src/main/java/com/jquiroga/mymovie/MainActivity.kt
+++ app/src/main/java/com/jquiroga/mymovie/MainActivity.kt
@@ -8,7 +8,6 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
@@ -20,81 +19,79 @@ import androidx.compose.foundation.lazy.grid.GridCells
 import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
 import androidx.compose.foundation.lazy.grid.items
 import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Menu
 import androidx.compose.material.icons.filled.PlayCircle
-import androidx.compose.material3.Button
+import androidx.compose.material.icons.filled.Search
+import androidx.compose.material.icons.filled.Share
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
-import androidx.compose.material3.TextField
+import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.saveable.rememberSaveable
-import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
 import coil.compose.AsyncImage
 import com.jquiroga.mymovie.ui.theme.MyMovieTheme
 
 class MainActivity : ComponentActivity() {
+    @OptIn(ExperimentalMaterial3Api::class)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             MyMovieTheme {
                 // A surface container using the 'background' color from the theme
                 Surface(color = MaterialTheme.colorScheme.background) {
-                    //getter = value
-                    //setter = onValueChange
-                    val (value , onValueChange) = rememberSaveable { mutableStateOf("") }
-                    StateSample(
-                        value = value,
-                        onValueChange = onValueChange
-                    )
+                    Scaffold(
+                        topBar = {
+                            TopAppBar(
+                                title = { Text(text = stringResource(id = R.string.app_name)) },
+                                navigationIcon = {
+                                    IconButton(onClick = { /*TODO*/ }) {
+                                        Icon(
+                                            imageVector = Icons.Default.Menu,
+                                            contentDescription = null
+                                        )
+                                    }
+                                },
+                                actions = {
+                                    IconButton(onClick = { /*TODO*/ }) {
+                                        Icon(
+                                            imageVector = Icons.Default.Search,
+                                            contentDescription = null
+                                        )
+                                    }
+                                    IconButton(onClick = { /*TODO*/ }) {
+                                        Icon(
+                                            imageVector = Icons.Default.Share,
+                                            contentDescription = null
+                                        )
+                                    }
+                                }
+                            )
+                        }
+                    ) { padding ->
+                        MediaList(modifier = Modifier.padding(padding))
+                    }
                 }
             }
         }
     }
 }
 
-@Composable
-fun StateSample(value: String, onValueChange: (String) -> Unit) {
-    Column(
-        modifier = Modifier
-            .fillMaxSize()
-            .padding(64.dp),
-        verticalArrangement = Arrangement.Center
-    ) {
-        TextField(
-            value = value,
-            onValueChange = { onValueChange(it) },
-            modifier = Modifier.fillMaxWidth()
-        )
-        Text(
-            text = value,
-            modifier = Modifier
-                .fillMaxWidth()
-                .background(Color.LightGray)
-                .padding(8.dp)
-        )
-        Button(
-            onClick = { onValueChange("") },
-            modifier = Modifier.fillMaxWidth(),
-            enabled = value.isNotEmpty()
-        ) {
-            Text(text = "Clear")
-        }
-    }
-}
-
 //@Preview
 @Composable
-fun MediaList() {
-        LazyVerticalGrid(
+fun MediaList(modifier: Modifier = Modifier) {
+    LazyVerticalGrid(
         contentPadding = PaddingValues(2.dp),
-        columns = GridCells.Adaptive(150.dp)
+        columns = GridCells.Adaptive(150.dp),
+        modifier = modifier
     ) {
         items(getMedia()) { item ->
             MediaListItem(item, Modifier.padding(2.dp))
